7df7f731cd3180f4e844ea55015eb8e3e5e4bf6f
Mohammad-Taleb <akamohdtaleb@gmail.com>
2025-04-15 13:23:00 +0300

    Add function <> adding the addtask function

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,8 @@
-const taskInput = document.getElementById('task-input')
-const addButton = document.getElementById('add-task-btn')
-const taskList = document.getElementById('task-list')
+const taskInput = document.getElementById('task-input');
+const addButton = document.getElementById('add-task-btn');
+const taskList = document.getElementById('task-list');
+
+function addTask(){
+  const taskText = taskInput.value.trim();
+  if (taskText === '') return;
+}
